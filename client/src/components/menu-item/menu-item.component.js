94399e6f1090b06e0fc979c3cf9a47e33a71db41
Jason McElvain <jasonom2@yahoo.com>
2021-07-25 19:12:03 -0500

    updating app with shop page with collection item component and preview component

--- client/src/components/menu-item/menu-item.component.js
+++ client/src/components/menu-item/menu-item.component.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import './menu-item.scss'
+import {withRouter} from 'react-router-dom';
 
 
-const MenuItem = ({title , imageUrl, size}) => {
+const MenuItem = ({title , imageUrl, size, history, link, match}) => {
   return (
     <>
-       <div  className={`${size} menu-item`}>
+       <div  className={`${size} menu-item`} onClick= {()=> history.push(`${match.url}${link}`) } >
             <div className="background-image" style={{backgroundImage: `url(${imageUrl})`}} />
      
             <div className="content">
@@ -17,4 +18,4 @@ const MenuItem = ({title , imageUrl, size}) => {
   )
 }
 
-export default MenuItem
+export default withRouter(MenuItem);
--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Switch, BrowserRouter, Route } from 'react-router-dom';
+import Homepage from './pages/homepage/homepage'
 import './index.css';
-import App from './App';
+import ShopPage from './pages/shop/shop.component'
+
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      <BrowserRouter>
+        <Switch>
+          <Route exact path='/' component={Homepage} />
+          <Route path ='/shop' component={ShopPage} />
+        </Switch>  
+      </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
